254b752b16b606af32e1b54681fdc3b8ecee2b2f
TYLY24 <90430357+TYLY24@users.noreply.github.com>
2024-11-06 09:36:04 +0700

    AudioSetting

--- Assets/Code/ShopSettingExitUI.cs
+++ Assets/Code/ShopSettingExitUI.cs
@@ -1,25 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.Audio;
 public class ShopSettingExitUI : MonoBehaviour
 {
-    public GameObject ExitPanel,Mainmenu,Shop;
+    public GameObject ExitPanel,Mainmenu,Shop,Setting;
+    [SerializeField] AudioManager audioManager;
+    [SerializeField] AudioMixer Mymixer;
+    [SerializeField] Slider musicSlider;
     // Start is called before the first frame update
-    void Start()
+    
+// --------------------------SETTING PART---------------------
+    public void SettingButton()
     {
+        audioManager.PlayVfx(audioManager.ButtonBig);
+        Setting.SetActive(true);
+        Mainmenu.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+     public void ExitSetting()
+    {
+        audioManager.PlayVfx(audioManager.ButtonSmall);
+        Time.timeScale = 1;
+        Setting.SetActive(false);
+        Mainmenu.SetActive(true);
         
     }
 
+// --------------------------SHOP PART---------------------
+
     public void ShopButton()
     {
+        audioManager.PlayVfx(audioManager.ButtonBig);
         Shop.SetActive(true);
         Mainmenu.SetActive(false);
         Time.timeScale = 0;
     }
     // Update is called once per frame
+
+// --------------------------EXIT PART---------------------
+
     public void ExitButton()
     {
+        audioManager.PlayVfx(audioManager.ButtonBig);        
         ExitPanel.SetActive(true);
         Mainmenu.SetActive(false);
         Time.timeScale = 0;
@@ -27,6 +52,7 @@ public class ShopSettingExitUI : MonoBehaviour
 
     public void ExitUnCheck()
     {
+        audioManager.PlayVfx(audioManager.ButtonSmall);
         Time.timeScale = 1;
         ExitPanel.SetActive(false);
         Mainmenu.SetActive(true);
@@ -35,6 +61,7 @@ public class ShopSettingExitUI : MonoBehaviour
 
     public void ExitCheck()
     {
+        audioManager.PlayVfx(audioManager.ButtonBig);
         Application.Quit();
     }
 
