bc92850f517ed69172f4c2c69e5cbe22d9caafc4
TYLY24 <90430357+TYLY24@users.noreply.github.com>
2024-11-06 10:30:20 +0700

    done Volume

--- Assets/Code/ShopSettingExitUI.cs
+++ Assets/Code/ShopSettingExitUI.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 public class ShopSettingExitUI : MonoBehaviour
 {
-    public GameObject ExitPanel,Mainmenu,Shop,Setting;
+    public GameObject ExitPanel,Mainmenu,Shop,Setting,ClearDataYN;
     [SerializeField] AudioManager audioManager;
     [SerializeField] AudioMixer Mymixer;
-    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider musicSlider,masterSlider,SfxSlider;
     // Start is called before the first frame update
     
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("musicVol"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            VolumeMusic();
+            VolumeSfx();
+            VolumeMaster();
+        }
+    }
+
+
+
 // --------------------------SETTING PART---------------------
     public void SettingButton()
     {
@@ -19,7 +36,23 @@ public class ShopSettingExitUI : MonoBehaviour
         Mainmenu.SetActive(false);
         Time.timeScale = 0;
     }
+    // ------Clear PART------------
+    public void ClearData()
+    {
+        ClearDataYN.SetActive(true);
+    }
+    public void No()
+    {
+        ClearDataYN.SetActive(false);
+    }
+    public void Yes()
+    {
+        PlayerPrefs.DeleteAll();
+        Time.timeScale = 1;
+        SceneManager.LoadScene("GamePlay");
+    }
 
+    // ----------------------------
      public void ExitSetting()
     {
         audioManager.PlayVfx(audioManager.ButtonSmall);
@@ -28,6 +61,37 @@ public class ShopSettingExitUI : MonoBehaviour
         Mainmenu.SetActive(true);
         
     }
+
+    public void VolumeMusic()
+    {
+        float Vol=musicSlider.value;
+        Mymixer.SetFloat("MusicVolume",Mathf.Log10(Vol)*20);
+        PlayerPrefs.SetFloat("musicVol",Vol);
+    }
+    public void VolumeSfx()
+    {
+        float Vol=SfxSlider.value;
+        Mymixer.SetFloat("SfxVolume",Mathf.Log10(Vol)*20);
+        PlayerPrefs.SetFloat("sfxVol",Vol);
+    }
+    public void VolumeMaster()
+    {
+        float Vol=masterSlider.value;
+        Mymixer.SetFloat("MasterVolume",Mathf.Log10(Vol)*20);
+        PlayerPrefs.SetFloat("masterVol",Vol);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value=PlayerPrefs.GetFloat("musicVol");
+        SfxSlider.value=PlayerPrefs.GetFloat("sfxVol");
+        masterSlider.value=PlayerPrefs.GetFloat("masterVol");
+        VolumeMusic();
+        VolumeSfx();
+        VolumeMaster();
+    }
+
+
 
 // --------------------------SHOP PART---------------------
 
